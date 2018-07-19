25ed77129eaf718b9982586a62bbb5fe616610fa
Marat765 <marat76593@gmail.com>
2018-07-20 00:47:15 +0300

    Правки

--- js/backend.js
+++ js/backend.js
@@ -3,33 +3,29 @@
 (function () {
   window.backend = {};
 
-  window.backend.load = function (onLoad) {
+  window.backend.load = function (onLoad, onError) {
     var URL = 'https://js.dump.academy/keksobooking/data';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-    xhr.timeout = 4000;
+    xhr.timeout = 10000;
 
     xhr.addEventListener('load', function () {
       switch (xhr.status) {
         case 200:
           onLoad(xhr.response);
           break;
-        // default:
-        //   onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        default:
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-
-    // xhr.addEventListener('error', function () {
-    //   onError('Произошла ошибка соединения');
-    // });
-
-    // xhr.addEventListener('timeout', function () {
-    //   onError('Запрос не успел выполниться за время ' + xhr.timeout + ' мс');
-    // });
-
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за время ' + xhr.timeout + ' мс');
+    });
     xhr.open('GET', URL);
     xhr.send();
-
   };
 
   window.backend.save = function (data, onLoad, onError) {
@@ -46,17 +42,13 @@
           onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
-
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
     });
-
     xhr.addEventListener('timeout', function () {
       onError('Данные не успели отправиться за время ' + xhr.timeout + ' мс');
     });
-
     xhr.open('POST', URL);
     xhr.send(data);
   };
-
 })();
