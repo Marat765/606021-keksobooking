b2d8c7a385e8fdca33b6a9062e481e6c9382ebe9
Marat765 <marat76593@gmail.com>
2018-07-20 17:09:03 +0300

    Исправления

--- js/map.js
+++ js/map.js
@@ -124,6 +124,9 @@
     node.style.fontSize = '24px';
     node.textContent = errorMessage;
     document.body.insertAdjacentElement('afterbegin', node);
+    setTimeout(function () {
+      document.body.children[0].remove();
+    }, 10000);
   };
 
   function onSuccess(cardsArray) {
