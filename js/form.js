7fd31cc8469e351a5900f33de8047ea24f332598
Marat765 <marat76593@gmail.com>
2018-07-28 15:42:51 +0300

    Правки

--- js/form.js
+++ js/form.js
@@ -16,65 +16,136 @@
     house: PRICE_MIN_HOUSE,
     bungalo: PRICE_MIN_BUNGALO
   };
+  var FOR_1_GUEST = 0;
+  var FOR_2_GUESTS = 1;
+  var FOR_3_GUESTS = 2;
+  var NOT_FOR_GUESTS = 3;
+  var COMPLIANCES_ROOMS_CAPACITIES = [
+    [FOR_2_GUESTS, FOR_3_GUESTS, NOT_FOR_GUESTS],
+    [FOR_3_GUESTS, NOT_FOR_GUESTS],
+    [NOT_FOR_GUESTS],
+    [FOR_1_GUEST, FOR_2_GUESTS, FOR_3_GUESTS]
+  ];
 
   var adForm = document.querySelector('.ad-form');
   var map = document.querySelector('.map');
   var successPopup = document.querySelector('.success');
   var mapPinMain = document.querySelector('.map__pin--main');
   var capacitySelect = adForm.querySelector('#capacity');
-  var submit = adForm.querySelector('.ad-form__submit');
-  var titleInput = adForm.querySelector('#title');
-  var roomNumberSelect = adForm.querySelector('#room_number');
 
-  function showError(element, condition, message) {
-    if (condition) {
-      element.style.borderColor = 'red';
-      element.setCustomValidity(message);
-    } else {
-      element.setCustomValidity('');
+  function CustomValidation() {}
+  CustomValidation.prototype = {
+  // Установим пустой массив сообщений об ошибках
+    invalidities: [],
+    // Метод, проверяющий валидность
+    checkValidity: function (input) {
+      this.invalidities = [];
+      var validity = input.validity;
+      if (validity.rangeOverflow) {
+        var max = input.getAttribute('max');
+        this.addInvalidity('Максимальное значение ' + max);
+      }
+      if (validity.tooShort) {
+        var minLength = input.getAttribute('minlength');
+        this.addInvalidity('Минимальная длина ' + minLength);
+      }
+      if (validity.rangeUnderflow) {
+        var min = input.getAttribute('min');
+        this.addInvalidity('Минимальное значение ' + min);
+      }
+      if (validity.tooLong) {
+        var maxLength = input.getAttribute('maxlength');
+        this.addInvalidity('Максимальная длина ' + maxLength);
+      }
+      if (validity.valueMissing) {
+        this.addInvalidity('Обязательно для заполнения');
+      }
+    },
+    // Добавляем сообщение об ошибке в массив ошибок
+    addInvalidity: function (message) {
+      this.invalidities.push(message);
+    },
+    // Получаем общий текст сообщений об ошибках
+    getInvalidities: function () {
+      return this.invalidities.join('. \n');
     }
-  }
+  };
 
-  var selectType = adForm.querySelector('#type');
+  CustomValidation.prototype.getInvaliditiesForHTML = function () {
+    return this.invalidities.join('. <br>');
+  };
+  // Добавляем обработчик клика на кнопку отправки формы
+  var submit = document.querySelector('.ad-form__submit');
+  submit.addEventListener('click', function (evt) {
+  // Пройдёмся по всем полям
+    capacitySelect.style.cssText = '';
+    var stopSubmit = false;
+    var inputs = document.querySelectorAll('.ad-form input');
+    for (var j = 0; j < inputs.length; j++) {
+      var input = inputs[j];
+      input.style.cssText = '';
+      // Проверим валидность поля, используя встроенную в JavaScript функцию checkValidity()
+      if (!input.checkValidity()) {
+        var inputCustomValidation = new CustomValidation(); // Создадим объект CustomValidation
+        inputCustomValidation.checkValidity(input); // Выявим ошибки
+        var customValidityMessage = inputCustomValidation.getInvalidities(); // Получим все сообщения об ошибках
+        input.setCustomValidity(customValidityMessage); // Установим специальное сообщение об ошибке
+        // Добавим ошибки в документ
+        var customValidityMessageForHTML = inputCustomValidation.getInvaliditiesForHTML();
+        input.insertAdjacentHTML('afterend', '<p class="error-message">' + customValidityMessageForHTML + '</p>');
+        stopSubmit = true;
+        input.style.cssText = 'border: 1px solid red; box-shadow: 0 0 3px red;';
+      }
+    }
+    var index = capacitySelect.selectedIndex;
+    if (capacitySelect.options[index].disabled) {
+      stopSubmit = true;
+      capacitySelect.insertAdjacentHTML('afterend', '<p class="error-message">Измените количество мест</p>');
+      capacitySelect.style.cssText = 'border: 1px solid red; box-shadow: 0 0 3px red;';
+    }
+    setTimeout(function () {
+      var errorMessages = adForm.querySelectorAll('.error-message');
+      for (j = 0; j < errorMessages.length; j++) {
+        errorMessages[j].remove();
+      }
+    }, 5000);
+    if (stopSubmit) {
+      evt.preventDefault();
+    }
+  });
+
+  var selectTypeHouse = adForm.querySelector('#type');
   var priceHouse = adForm.querySelector('#price');
   function onSelectTypeChange() {
-    priceHouse.placeholder = COMPLIANCE_TYPE_PRICE[selectType.value];
-    priceHouse.min = COMPLIANCE_TYPE_PRICE[selectType.value];
+    priceHouse.placeholder = COMPLIANCE_TYPE_PRICE[selectTypeHouse.value];
+    priceHouse.min = COMPLIANCE_TYPE_PRICE[selectTypeHouse.value];
   }
-  selectType.addEventListener('change', onSelectTypeChange);
+  selectTypeHouse.addEventListener('change', onSelectTypeChange);
 
   var timeIn = adForm.querySelector('#timein');
   var timeOut = adForm.querySelector('#timeout');
-  function synchronizeTime(evt) {
+  function onSelectTimeChange(evt) {
     if (evt.target === timeIn) {
       timeOut.selectedIndex = evt.target.selectedIndex;
     } else {
       timeIn.selectedIndex = evt.target.selectedIndex;
     }
   }
-  timeIn.addEventListener('change', function (evt) {
-    synchronizeTime(evt);
-  });
-  timeOut.addEventListener('change', function (evt) {
-    synchronizeTime(evt);
-  });
+  timeIn.addEventListener('change', onSelectTimeChange);
+  timeOut.addEventListener('change', onSelectTimeChange);
 
-  function checkNumberOfGuests() {
-    switch (+roomNumberSelect.value) {
-      case 1:
-        showError(capacitySelect, (+capacitySelect.value === 2 || +capacitySelect.value === 3 || +capacitySelect.value === 0), 'Для одного гостя');
-        break;
-      case 2:
-        showError(capacitySelect, (+capacitySelect.value === 3 || +capacitySelect.value === 0), 'Не более двух гостей');
-        break;
-      case 3:
-        showError(capacitySelect, (+capacitySelect.value === 0), 'Не более трех гостей');
-        break;
-      case 100:
-        showError(capacitySelect, (!(+capacitySelect.value === 0)), 'Не для гостей');
-        break;
+  var roomNumberSelect = adForm.querySelector('#room_number');
+  function onSelectCapacityChange(evt) {
+    var capacitySelectOptions = adForm.querySelectorAll('#capacity option');
+    for (var j = 0; j < capacitySelectOptions.length; j++) {
+      capacitySelectOptions[j].disabled = false;
+    }
+    var capacities = COMPLIANCES_ROOMS_CAPACITIES[evt.target.selectedIndex];
+    for (j = 0; j < capacities.length; j++) {
+      capacitySelect.options[capacities[j]].disabled = true;
     }
   }
+  roomNumberSelect.addEventListener('change', onSelectCapacityChange);
 
   function deactivatePage() {
     var mapPins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
@@ -94,9 +165,11 @@
     for (i = 0; i < fieldsets.length; i++) {
       fieldsets[i].disabled = true;
     }
-    capacitySelect.style.borderColor = '#d9d9d3';
-    titleInput.style.borderColor = '#d9d9d3';
-    priceHouse.style.borderColor = '#d9d9d3';
+    var inputs = document.querySelectorAll('.ad-form input');
+    for (i = 0; i < inputs.length; i++) {
+      var input = inputs[i];
+      input.style.cssText = '';
+    }
     priceHouse.placeholder = PRICE_MIN_FLAT;
     priceHouse.min = PRICE_MIN_FLAT;
     window.map.loadPage();
@@ -127,17 +200,6 @@
 
   adForm.querySelector('.ad-form__reset').addEventListener('click', function () {
     deactivatePage();
-  });
-
-  submit.addEventListener('click', function () {
-    capacitySelect.style.borderColor = '#d9d9d3';
-    titleInput.style.borderColor = '#d9d9d3';
-    priceHouse.style.borderColor = '#d9d9d3';
-    checkNumberOfGuests();
-    showError(titleInput, (titleInput.value.length < 30 || titleInput.value.length > 100), 'Поле должно содержать от 30 до 100 символов');
-    if (!priceHouse.validity.valid) {
-      priceHouse.style.borderColor = 'red';
-    }
   });
 
   adForm.addEventListener('submit', function (evt) {
